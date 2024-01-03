728223e81c5bd58303fe8053b86cd0d846f2344a
Anna Karla <123194582+AnnaKB9997@users.noreply.github.com>
2024-01-03 13:37:16 -0300

    Add files via upload

---
+++ desafio.js
@@ -0,0 +1,22 @@
+// Desafio do ataque do herói utilizando classes
+
+class Heroi {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+  atacar() {
+    let ataque;
+    if (this.tipo == 'mago'){
+                ataque = 'magia';
+    } else if (this.tipo == 'guerreiro'){
+        ataque = 'espada';
+    } else if (this.tipo == 'monge'){
+        ataque = 'artes marciais';
+    } else if (this.tipo == 'ninja') {
+        ataque = 'surikhen';
+    }
+    console.log(`O ${this.tipo} atacou usando ${ataque}`)
+    }
+}
